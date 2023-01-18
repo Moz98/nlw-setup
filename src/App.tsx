1b114450b1e5be7803aaed6751b83e9fc825a584
Moz98 <marcosoliveirazacarias@gmail.com>
2023-01-18 15:56:37 -0300

    feat(summary): Adiciona o header e a tabela da table

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+import "./styles/globals.css";
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  );
+}
+
+export default App;
