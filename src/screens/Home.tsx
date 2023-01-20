1f389a46236081d5c821ecdfbcba72a86567fae2
Moz98 <marcosoliveirazacarias@gmail.com>
2023-01-20 10:21:55 -0300

    Cria o componente de DayHabits e o seu cabeçalho

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,14 @@
 import { View } from "react-native";
+import { HabitDay } from "../components/HabitDay";
+import { HabitDayHeader } from "../components/HabitDayHeader";
 import { Header } from "../components/Header";
 
 export const Home = () => {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+      <HabitDayHeader />
+      <HabitDay />
     </View>
   );
 };
