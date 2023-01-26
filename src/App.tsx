1f4b42e1274b93d12654a881dd7d57c7e791deb0
Moz98 <marcosoliveirazacarias@gmail.com>
2023-01-26 10:15:13 -0300

    feat(api): conceta a criação de hábitos a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
 import "./styles/globals.css";
+import "./lib/dayjs";
 
 function App() {
   return (
